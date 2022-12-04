a747e54b8ab9018edd2cbcb8b81e0664087bdd11
Lennard Moe <103784266+LennardMoe@users.noreply.github.com>
2022-12-04 14:52:16 +0100

    hosting test 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,7 @@ function Home() {
       transition={{ duration: 0.5 }}
     >
       <Styling>
-        <HomeButtons text='New Recipes' link='/createRecipe/' />
+        <HomeButtons text='New Recipesss' link='/createRecipe/' />
         <HomeButtons text='My Recipe' link='/MyRecipes/' />
         <HomeButtons text='View All' link='/allRecipesList/' />
       </Styling>
